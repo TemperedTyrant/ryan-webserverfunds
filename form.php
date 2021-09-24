89c37b5c91eb5ca30c9954506ea8be4ed82a9a3d
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-09-24 12:02:03 -0400

    Update form.php

--- form.php
+++ form.php
@@ -8,7 +8,19 @@
         <p>To return to the main page, <a href="index.html">Click here!</a></p>
         <h1>Dump of $_GET</h1>
         <p><?= var_dump($_GET) ?></p>
-        <h2>Dump of $_POST</h2>
+        <h1>Dump of $_POST</h1>
         <p><?= var_dump($_POST) ?></p>
+        <!--Make user friendly-->
+        <h2>Here are the results of your form!</h2>
+        <div style=background-color:#D3D3D3>
+        <ul>
+                <li>Mode: <?=$_POST["mode"];?></li> 
+                <li></li>
+                <li>Playstyle: <?=$_POST["Device"];?></li>
+                <li>Additional Details: <?=$_POST["Request"];?></li>
+                <li>Playstyle: <?=$_POST["Device"];?></li>
+                <li>Link: <?=$_POST["Software_link"];?></li>
+            </ul>        
+        </div>
     </body>
 </html>
