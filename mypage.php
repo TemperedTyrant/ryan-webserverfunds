e0800d8a703d4bfcd6cf08deb869a5ba74339bf0
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-09-24 11:07:04 -0400

    Update mypage.php

--- mypage.php
+++ mypage.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-My first PHP Page!-->
+<!--My first PHP Page!-->
 <html>
     <head>
         <title>Welcome to my first PHP Webpage for Fundamentals of Web Servers!</title>
@@ -7,4 +7,6 @@
     <body>
         <p>To return to the main page, <a href="index.html">Click here!</a></p>
         <p><?= var_dump($_SERVER) ?></p>
+        <h1>User-Agent Strint:</h1>
+        <p>Your User-Agent string is: <?php echo "$_HTTP_USER_AGENT";?><p>
 </html>
