746df7a0d396360896b21ec14d088443b3e68cde
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-09-24 10:56:28 -0400

    Added clickable links to other pages

--- mypage.php
+++ mypage.php
@@ -5,5 +5,6 @@
         <title>Welcome to my first PHP Webpage for Fundamentals of Web Servers!</title>
     </head>
     <body>
+        <p>To return to the main page, <a href="index.html">Click here!</a></p>
         <p><?= var_dump($_SERVER) ?></p>
 </html>
