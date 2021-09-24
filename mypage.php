a89c3e3442dbbbbc1e538f7a6eed029dfdda04c1
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-09-24 10:50:47 -0400

    Create mypage.php

---
+++ mypage.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<!-My first PHP Page!-->
+<html>
+    <head>
+        <title>Welcome to my first PHP Webpage for Fundamentals of Web Servers!</title>
+    </head>
+    <body>
+        <p><?= var_dump($_SERVER) ?></p>
+</html>
