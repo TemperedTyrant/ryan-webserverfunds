1f48a0157029621c9ec762ea99dc6e5b6cfce28a
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-10-12 10:55:54 -0400

    Added comments to midterm pages

--- response.php
+++ response.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<!--PHP resposne page for Midterm 10/12/2021-->
+<!-- PHP response page for Midterm 10/12/2021 -->
 <html>
     <head>
-        <title>PHP Response</title>
+        <title>PHP Response</title> <!-- Simple title -->
     </head>
     <body>
+        <!-- Display results of the form as requested in midterm instructions -->
         <p>You have requested <?=$_POST["Quantity"];?> <?=$_POST["product_name"];?>.<br></p>       
+        <!-- Add ways to return to previous pages -->
         <p>Return to main page <a href="index.html">here.</a></p>
         <p>Return to form page <a href="index.html">here.</a></p>
     </body>
