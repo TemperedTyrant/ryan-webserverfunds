3a33de2fb18c4ec495177c6bce19d3629a88bfe0
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-10-12 10:46:57 -0400

    Update response.php

--- response.php
+++ response.php
@@ -7,9 +7,11 @@
     <body>
         <p>To return to the main page, <a href="index.html">Click here!</a></p>
         <h2>Here are the results of your form!</h2>
+
+        <p>You have requested <?=$_POST["Quantity"];?> <?=$_POST["product_name"];?><br>
         <div style=background-color:#D3D3D3>
             <ul>
-                <li>Mode: <?=$_POST["product_name"];?></li> 
-                <li>Additional Details: <?=$_POST["Quantity"];?></li>
+                <li>Product: <?=$_POST["product_name"];?></li> 
+                <li>Quantity: <?=$_POST["Quantity"];?></li>
             </ul>        
         </div>
