b6fbe2b09d4999a9c47241b285d45cc89884c353
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-10-12 10:49:54 -0400

    update

--- response.php
+++ response.php
@@ -5,13 +5,8 @@
         <title>PHP Response</title>
     </head>
     <body>
-        <p>To return to the main page, <a href="index.html">Click here!</a></p>
-        <h2>Here are the results of your form!</h2>
-
-        <p>You have requested <?=$_POST["Quantity"];?> <?=$_POST["product_name"];?><br>
-        <div style=background-color:#D3D3D3>
-            <ul>
-                <li>Product: <?=$_POST["product_name"];?></li> 
-                <li>Quantity: <?=$_POST["Quantity"];?></li>
-            </ul>        
-        </div>
+        <p>You have requested <?=$_POST["Quantity"];?> <?=$_POST["product_name"];?>.<br></p>       
+        <p>Return to main page <a href="index.html">here.</a></p>
+        <p>Return to form page <a href="index.html">here.</a></p>
+    </body>
+</html>
