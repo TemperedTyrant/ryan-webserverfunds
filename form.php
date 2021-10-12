44345809b9b6f9da527817adf8c76313ac31d96b
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-10-12 12:09:08 -0400

    Update form.php

--- form.php
+++ form.php
@@ -23,7 +23,7 @@
                                         $type="software;";
                                     }
                                     else {
-                                        $type= "no Software;"
+                                        $type= "no Software;";
                                     }
                                     if(isset($_POST["Hardware"])){
                                         echo "Hardware ";
@@ -68,6 +68,32 @@
         mysqli_close($conn);
         ?>
 
+        <h2>Here are the results from table 'requests' in database 'ryan_info'</h2>                            
+        <?php 
+        $servername = "localhost";
+        $username = "ryan";
+        $password = "Class!23";
+        $dbname = "ryan_info";
+        //Create Connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        //Check Connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
+        $sql = "SELECT request_id, type, mode, device FROM requests";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            //output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "id: " . $row["request_id"]. " - Type: " . $row["type"]. " - Mode: " . $row["mode"]. " - Device: " . $row["device"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+        mysqli_close($conn);
+       ?>
     </body>
 </html>
