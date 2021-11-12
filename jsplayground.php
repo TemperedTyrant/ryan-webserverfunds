b00059ec87611f656cc6f2adfdf1c89a4df2b5e9
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-11-12 11:28:55 -0500

    Update jsplayground.php

--- jsplayground.php
+++ jsplayground.php
@@ -58,9 +58,9 @@
         
         <?php
             if(isset($_POST['toggle'])) {
-                $output = `gpio toggle 0`;
-                $logic = `gpio read 0`;
-                echo "The LED logic level is $logic";
+                exec("gpio toggle 0");
+                exec("$logic = `gpio read 0`");
+                echo("State of LED is $logic");
             }
         ?>
 
