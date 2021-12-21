9d14fcb38fa5436799b8cd26fb13d3251d7d5b32
Ryan Masson Hiscock <ryan.massonhiscock@dawsoncollege.qc.ca>
2021-12-21 11:16:18 -0500

    work

--- fakeform.php
+++ fakeform.php
@@ -16,6 +16,8 @@
             $password = "Class!23";
             $dbname = "ryan_info";
 
+            $url = "https://www.google.ca/search?q=";
+
             $search = htmlspecialchars ($_POST["Search"]);
 
             //Create Connection
@@ -27,10 +29,21 @@
 
             $sql = "INSERT INTO finalexam (ipaddr, search) VALUES  ('{$_SERVER['REMOTE_ADDR']}', '{$search}')";
             $result = mysqli_query($conn, $sql);
+
+            header("location: " . $url, $search);
         
             mysqli_close($conn);
         ?>
 
         <?php
             echo $_SERVER['REMOTE_ADDR'];
+        ?>
+        <?php
+            $val = array (
+            'weather' => 'Sunny',
+            'time' => '1:30 pm',
+            );
+            if(file_put_contents('file.txt', print_r($day, true))){
+                header("location: message.php");
+            }
         ?>
